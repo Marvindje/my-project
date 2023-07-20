80fba711a7c7146b5399ef49f660aa7ea75f79c4
Marvindje <marvindje@wilder.school>
2023-07-21 00:54:32 +0200

    tailwind resolu

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './styles/App.module.css';
 import './styles/Navbar.module.css';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
