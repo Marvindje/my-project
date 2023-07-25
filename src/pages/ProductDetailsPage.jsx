170424486aea350f7727291511a4ea2c30ae8463
Marvindje <marvindje@wilder.school>
2023-07-25 16:14:39 +0200

    essai

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { FAKE_PRODUCTS } from "../../fake_api/products"
+import { IMAGES } from '../../assets/images.js'; // Importez les données de images.js
 
 const ProductDetailsPage = () => {
   const [currentProduct, setCurrentProduct] = useState({})
@@ -8,7 +8,7 @@ const ProductDetailsPage = () => {
   
   useEffect(() => {
     if(id){
-      const findProduct = FAKE_PRODUCTS.find(product => product.id === id);
+      const findProduct = IMAGES.find(product => product.id === id); // Utilisez les données de images.js au lieu de l'API faker
       setCurrentProduct(findProduct)
     }
   }, [id])
@@ -20,8 +20,7 @@ const ProductDetailsPage = () => {
         <div className="flex flex-col items-center justify-center">
           <h3 className="text-xl font-semibold mb-2">{currentProduct.name}</h3>
           <img className="w-full h-64 object-cover mb-4" src={currentProduct.image} alt={currentProduct.name} />
-          <p className="text-gray-700 mb-2">Category: {currentProduct.category}</p>
-          <p className="text-gray-700 mb-2">Type: {currentProduct.type}</p>
+          <p className="text-gray-700 mb-2">Category: {currentProduct.type}</p> {/* Ici, j'ai supposé que le type est la catégorie */}
           <p className="text-lg font-bold mb-2">Price: {currentProduct.price}</p>
           <p className="text-gray-700 mb-2">Description: {currentProduct.description}</p>
         </div>
