3547c92129521c1284f2e48980af3884cdc5cf37
Marvindje <marvindje@wilder.school>
2023-07-25 11:58:59 +0200

    navbar

---
+++ src/assets/banque d'images/images.js
@@ -0,0 +1,62 @@
+export const IMAGES = [
+    {
+        "id": "1",
+        "price": 10.99,
+        "type": "t-shirt",
+        "description": "Description for anomaly-WWesmHEgXDs-unsplash.jpg",
+        "name": "anomaly-WWesmHEgXDs-unsplash",
+        "image": require('./banque dimages/anomaly-WWesmHEgXDs-unsplash.jpg')
+    },
+    {
+        "id": "2",
+        "price": 10.99,
+        "type": "t-shirt",
+        "description": "Description for bao-bao-TysFvOl78u0-unsplash.jpg",
+        "name": "bao-bao-TysFvOl78u0-unsplash",
+        "image": require('./banque dimages/bao-bao-TysFvOl78u0-unsplash.jpg')
+    },
+    {
+        "id": "3",
+        "price": 10.99,
+        "type": "accessories",
+        "description": "Description for camila-seves-espasandin-o4RpLOAF2OA-unsplash.jpg",
+        "name": "camila-seves-espasandin-o4RpLOAF2OA-unsplash",
+        "image": require('./banque dimages/camila-seves-espasandin-o4RpLOAF2OA-unsplash.jpg')
+    },
+   
+    {
+        "id": "4",
+        "price": 10.99,
+        "type": "accessories",
+        "description": "Description for lola-rose-840tjesh9ww-unsplash.jpg",
+        "name":"lola-rose-840tjesh9ww-unsplash",
+        "image": require('./banque dimages/lola-rose-840tjesh9ww-unsplash.jpg')
+    },
+
+    {
+        "id": "5",
+        "price":15.99,
+        "type": "accessories",
+        "description": "Description for kelsey-curtis-oB_d7fqHHiU-unsplash.jpg",
+        "name":"kelsey-curtis-oB_d7fqHHiU-unsplash",
+        "image": require('./banque dimages/kelsey-curtis-oB_d7fqHHiU-unsplash.jpg')
+    },
+
+    {
+        "id": "6",
+        "price":15.99,
+        "type": "accessories",
+        "description": "Description for jesus-eca-vr0q87dQQfA-unsplash.jpg",
+        "name":"jesus-eca-vr0q87dQQfA-unsplash",
+        "image": require('./banque dimages/jesus-eca-vr0q87dQQfA-unsplash.jpg')
+    },
+
+    {
+        "id": "7",
+        "price":15.99,
+        "type": "shirts",
+        "description": "Description for pesce-huang-vJ9DTh6zkR4-unsplash.jpg",
+        "name":"pesce-huang-vJ9DTh6zkR4-unsplash",
+        "image": require('./banque dimages/pesce-huang-vJ9DTh6zkR4-unsplash.jpg')
+    },
+];
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
-
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeMenu, setActiveMenu] = useState(null);
@@ -24,24 +23,28 @@ export const Navbar = () => {
   const renderSubMenu = (menuName) => {
     if (menuName === 'Hommes') {
       return (
-        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-white p-2">
-          <Link to="/hommes/tshirts" className="block text-gray-800 hover:text-blue-500">T-shirts</Link>
-          <Link to="/hommes/chemises" className="block text-gray-800 hover:text-blue-500">Chemises</Link>
+        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-gray-800 p-2 rounded shadow-lg">
+          <Link to="/hommes/tshirts" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">T-shirts</Link>
+          <Link to="/hommes/chemises" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Chemises</Link>
+          <Link to="/hommes/sweatshirts" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Sweatshirts</Link>
         </div>
       );
     } else if (menuName === 'Femmes') {
       return (
-        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-white p-2">
-          <Link to="/femmes/tshirts" className="block text-gray-800 hover:text-blue-500">T-shirts</Link>
-          <Link to="/femmes/chemises" className="block text-gray-800 hover:text-blue-500">Chemises</Link>
+        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-gray-800 p-2 rounded shadow-lg">
+          <Link to="/femmes/tshirts" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">T-shirts</Link>
+          <Link to="/femmes/chemises" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Chemises</Link>
+          <Link to="/femmes/sweatshirts" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Sweatshirts</Link>
         </div>
       );
     } else if (menuName === 'Accessoires') {
       return (
-        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-white p-2">
-          <Link to="/accessoires/sacsamain" className="block text-gray-800 hover:text-blue-500">Sacs</Link>
-          <Link to="/accessoires/sacsados" className="block text-gray-800 hover:text-blue-500">Backpacks</Link>
-          <Link to="/accessoires/casquettes" className="block text-gray-800 hover:text-blue-500">Casquettes</Link>
+        <div className="mb-2 transition-all duration-200 ease-in-out absolute bg-gray-800 p-2 rounded shadow-lg">
+          <Link to="/accessoires/sacsamain" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Sacs</Link>
+          <Link to="/accessoires/sacsados" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Backpacks</Link>
+          <Link to="/accessoires/casquettes" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Casquettes</Link>
+          <Link to="/accessoires/bijoux" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Bijoux</Link>
+          <Link to="/accessoires/lunettes" className="block text-white hover:text-blue-500 transition-all duration-200 transform hover:scale-110">Lunettes</Link>
         </div>
       );
     }
