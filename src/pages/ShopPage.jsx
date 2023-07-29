98939b83c508598542333af4b1831f6cfb291c51
Marvindje <marvindje@wilder.school>
2023-07-29 18:23:40 +0200

    fix key prop issue

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -6,25 +6,24 @@ import articles from '../data/articles.js';
 const ShopPage = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-cover p-8 overflow-auto" style={{ height: '100vh', gridAutoRows: 'minmax(100px, auto)', justifyContent: 'center' }}>
-      {articles.map(article => (
-        <Parallax y={[-20, 20]}>
-          <motion.div 
-            key={article.id}
-            className="rounded overflow-hidden shadow-lg bg-white"
-            whileHover={{ scale: 1.05 }}
-            style={{ margin: '20px', maxWidth: '300px' }}
-          >
-            <img className="w-full h-auto object-cover" src={article.image} alt={article.name} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-2xl mb-2">{article.name}</div>
-              <p className="text-gray-800 text-lg">{article.description}</p>
-            </div>
-            <div className="px-6 py-4">
-              <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-lg font-semibold text-gray-800 mr-2 mb-2">${article.price}</span>
-            </div>
-          </motion.div>
-        </Parallax>
-      ))}
+    {articles.map(article => (
+      <Parallax y={[-20, 20]} key={article.id}>
+        <motion.div 
+          className="rounded overflow-hidden shadow-lg bg-white"
+          whileHover={{ scale: 1.05 }}
+          style={{ margin: '20px', maxWidth: '300px' }}
+        >
+          <img className="w-full h-auto object-cover" src={article.image} alt={article.name} />
+          <div className="px-6 py-4">
+            <div className="font-bold text-2xl mb-2">{article.name}</div>
+            <p className="text-gray-800 text-lg">{article.description}</p>
+          </div>
+          <div className="px-6 py-4">
+            <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-lg font-semibold text-gray-800 mr-2 mb-2">${article.price}</span>
+          </div>
+        </motion.div>
+      </Parallax>
+    ))}
     </div>
   );
 };
