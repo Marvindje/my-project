32f24e94a2b5dd7c0bb5e68ed993023580d66ab7
Marvindje <marvindje@wilder.school>
2023-07-30 19:04:30 +0200

    modifs homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SearchBar from '../components/SearchBar';
+import { motion } from 'framer-motion';
 import ProductCategories from '../components/ProductCategories';
 
 const Homepage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-6">
-      <SearchBar />
-      <ProductCategories />
-      <Link to="/products" className="inline-block mt-4 bg-blue-500 text-white px-4 py-2 rounded">Shop Now</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+      <div className="text-center text-gray-800">
+        <motion.h1 
+          className="text-6xl font-bold mb-4"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Welcome to Our Shop
+        </motion.h1>
+        <motion.p 
+          className="text-xl mb-8"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+        >
+          Explore our collection of amazing products
+        </motion.p>
+      </div>
+      <ProductCategories className="mb-8" />
+      <Link to="/products" className="inline-block mt-4 bg-white text-gray-700 px-6 py-3 rounded-full font-bold text-xl hover:bg-gray-700 hover:text-white transition-colors duration-300">Shop Now</Link>
     </div>
   );
 };
--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { Parallax } from 'react-scroll-parallax';
 import { Link } from 'react-router-dom'; 
 import articles from '../data/articles.js'; 
+import SearchBar from '../components/SearchBar';
 
 const ShopPage = () => {
+  const [searchValue, setSearchValue] = useState('');
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-cover p-8 overflow-auto" style={{ gridAutoRows: 'auto', justifyContent: 'center' }}>
-    {articles.map(article => (
-      <Parallax y={[-20, 20]} key={article.id}>
-        <Link to={`/products/${article.id}`}>
-          <motion.div 
-            className="rounded overflow-hidden shadow-lg bg-white"
-            whileHover={{ scale: 1.05 }}
-            style={{ margin: '20px', maxWidth: '300px' }}
-          >
-            <img className="w-full h-auto object-cover" src={article.image} alt={article.name} />
-            <div className="px-6 py-4">
-              <div className="font-bold text-2xl mb-2">{article.name}</div>
-              <p className="text-gray-800 text-lg">{article.description}</p>
-            </div>
-            <div className="px-6 py-4">
-              <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-lg font-semibold text-gray-800 mr-2 mb-2">${article.price}</span>
-            </div>
-          </motion.div>
-        </Link>
-      </Parallax>
-    ))}
+    <div>
+      <div className="flex justify-center mt-6">
+        <SearchBar onChangeSearchValue={setSearchValue} />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-cover p-8 overflow-auto" style={{ gridAutoRows: 'auto', justifyContent: 'center' }}>
+        {articles.filter(article => article.name.toLowerCase().includes(searchValue.toLowerCase())).map(article => (
+          <Parallax y={[-20, 20]} key={article.id}>
+            <Link to={`/products/${article.id}`}>
+              <motion.div 
+                className="rounded overflow-hidden shadow-lg bg-white"
+                whileHover={{ scale: 1.05 }}
+                style={{ margin: '20px', maxWidth: '300px' }}
+              >
+                <img className="w-full h-auto object-cover" src={article.image} alt={article.name} />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-2xl mb-2">{article.name}</div>
+                  <p className="text-gray-800 text-lg">{article.description}</p>
+                </div>
+                <div className="px-6 py-4">
+                  <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-lg font-semibold text-gray-800 mr-2 mb-2">${article.price}</span>
+                </div>
+              </motion.div>
+            </Link>
+          </Parallax>
+        ))}
+      </div>
     </div>
   );
 };
 
 
 export default ShopPage;
+
